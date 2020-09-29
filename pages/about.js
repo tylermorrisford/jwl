d246eb032e415dc2db46f6efcac52c242a3444ab
Tyler Morris Ford <tylerford@gmail.com>
2020-09-28 23:27:07 -0600

    fix footer border

--- pages/about.js
+++ pages/about.js
@@ -98,7 +98,7 @@ export default function Home() {
         footer {
           width: 100%;
           height: 100px;
-          border-top: 1px solid #eaeaea;
+          border-top: 1px solid black;
           display: flex;
           justify-content: center;
           align-items: center;
